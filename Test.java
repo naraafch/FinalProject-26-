4c4ac58c24c0ff77d3da7cae2a7785eeb94f26fc
kamilakhairunnisa <erinazkk@gmail.com>
2025-09-25 10:14:04 +0700

    satu

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main (String [] args) {
         System.out.println("Haiiiiiiiiii");
+        System.out.println("hai nara ini erinaaaaa");
+
     }
     
 }
