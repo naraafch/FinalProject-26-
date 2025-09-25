4c49cf02f8548a76ace78a1fb5e27b5c0f211f6b
rochiszigmakinara <rochiszigmakinara@gmail.com>
2025-09-25 10:19:00 +0700

    2

--- Test.java
+++ Test.java
@@ -1,12 +1,9 @@
 public class Test {
     public static void main (String [] args) {
         System.out.println("Haiiiiiiiiii");
-<<<<<<< HEAD
         System.out.println("ppppppppppp");
-=======
         System.out.println("hai nara ini erinaaaaa");
 
->>>>>>> 4c4ac58c24c0ff77d3da7cae2a7785eeb94f26fc
     }
     
 }
